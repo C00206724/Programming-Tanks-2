82a99986eff10e02820f8799e95c9ed2082d6f57
(Student) - Conor O Tuathail <C00206724@itcarlow.ie>
2017-04-06 21:34:03 +0100

    commiting project

---
+++ src/systems/AiControlSystem.cpp
@@ -0,0 +1,67 @@
+#include "systems/AiControlSystem.h"
+
+
+AiControlSystem::AiControlSystem()
+{
+}
+
+void AiControlSystem::configure(entityx::EventManager& events)
+{  
+   events.subscribe<EvReportPlayerId>(*this);
+   events.subscribe<entityx::ComponentAddedEvent<Ai>>(*this);
+   events.subscribe<entityx::ComponentAddedEvent<Wall>>(*this);
+}
+
+void AiControlSystem::receive(const EvReportPlayerId& e)
+{
+	m_playerId = e.m_playerId;
+}
+
+void AiControlSystem::receive(const entityx::ComponentAddedEvent<Ai>& e)
+{
+    Ai::Handle ai = e.component;
+    m_tankAi.reset(new TankAi(m_obstacles, ai->m_id));	// id of turret
+	m_tankAi.reset(new TankAi(m_waypoints, ai->m_id));  // 
+}
+
+void AiControlSystem::receive(const entityx::ComponentAddedEvent<Wall>& e)
+{
+	entityx::Entity ent = e.entity;
+	Volume::Handle wallVol = ent.component<Volume>();
+	Position::Handle wallPos = ent.component<Position>();
+	Display::Handle wallDisplay = ent.component<Display>();
+	
+	sf::CircleShape circle(wallVol->m_box.getRect().width * 1.5f);
+	circle.setOrigin(circle.getRadius(), circle.getRadius());
+	circle.setPosition(wallPos->m_position);
+	m_obstacles.push_back(circle);	
+}
+
+//A copy of the wall receive
+void AiControlSystem::receive(const entityx::ComponentAddedEvent<WaypointData>& e)
+{
+	entityx::Entity ent = e.entity;
+	Volume::Handle wayPointVol = ent.component<Volume>();
+	Position::Handle waypointPos = ent.component<Position>();
+	Display::Handle waypointDisplay = ent.component<Display>();
+
+	sf::CircleShape circle(wayPointVol->m_box.getRect().width * 1.5f);
+	circle.setOrigin(circle.getRadius(), circle.getRadius());
+	circle.setPosition(waypointPos->m_position);
+	m_waypoints.push_back(circle);
+}
+
+void AiControlSystem::update(entityx::EntityManager& entities,
+                             entityx::EventManager& events,
+                             double dt)
+{
+   Ai::Handle ai;
+   for (entityx::Entity entity : entities.entities_with_components(ai))
+   {
+	   m_tankAi->update(m_playerId, 
+		                    entity.id(),
+							entities, 
+							dt);
+  
+   }
+}
